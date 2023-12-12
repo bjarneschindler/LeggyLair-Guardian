426bdcbe967a824cd558d60394c484c623ade5be
Bjarne Schindler <bjarne.schindler@icloud.com>
2023-12-12 10:47:10 +0100

    ready deployment

--- src/app/(auth)/auth/auth-code-error/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AuthErrorPage() {
-  return (
-    <div>
-      <span>Authentication Error</span>
-      <p>An error occurred during authentication. Please try again.</p>
-    </div>
-  );
-}
